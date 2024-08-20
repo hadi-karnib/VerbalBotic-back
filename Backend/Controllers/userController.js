0004f4e3265aaf44a8995f19750ceb8bc5f96a59
hadi-karnib <hadikarnib03@gmail.com>
2024-08-21 01:56:11 +0300

    added get user chats function

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -102,3 +102,20 @@ export const deleteUser = async (req, res) => {
       .json({ message: "Something went wrong", error: err.message });
   }
 };
+
+export const getUserChats = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id).select("chat.messages");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user.chat.messages);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong", error: err.message });
+  }
+};
