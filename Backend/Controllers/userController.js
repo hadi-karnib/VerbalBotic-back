ca0cddc68bc84d3530465cf947c86a7da9298dd2
hadi-karnib <hadikarnib03@gmail.com>
2024-08-21 01:55:39 +0300

    added delete fumction

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -86,3 +86,19 @@ export const getUser = async (req, res) => {
       .json({ message: "Something went wrong", error: err.message });
   }
 };
+
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong", error: err.message });
+  }
+};
