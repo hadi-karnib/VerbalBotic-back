b538333948f7919892d44969fb8873ceb843767c
hadi-karnib <hadikarnib03@gmail.com>
2024-08-21 02:35:11 +0300

    created update afterchatgpt route

--- Backend/Routes/messageRoutes.js
+++ Backend/Routes/messageRoutes.js
@@ -11,3 +11,7 @@ const router = express.Router();
 router.post("/", authMiddleware, createVoiceNote);
 
 router.patch("/:messageId/analysis", authMiddleware, updateAfterAnalysis);
+
+router.patch("/:messageId/chatgpt", authMiddleware, updateAfterChatGPT);
+
+export default router;
