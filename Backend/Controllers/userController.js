c6df8df510c00f1d86fc98ab0ba47f8a70f30629
hadi-karnib <hadikarnib03@gmail.com>
2024-08-21 01:55:18 +0300

    added get users function

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -70,3 +70,19 @@ export const getAllUsers = async (req, res) => {
       .json({ message: "Something went wrong", error: err.message });
   }
 };
+
+export const getUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong", error: err.message });
+  }
+};
