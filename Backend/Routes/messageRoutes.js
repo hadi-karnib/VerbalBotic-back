3c4887d695b2ae66aa13089ce30b701e04aac318
hadi-karnib <hadikarnib03@gmail.com>
2024-08-21 02:33:46 +0300

    imported functions for messages

---
+++ Backend/Routes/messageRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import {
+  createVoiceNote,
+  updateAfterAnalysis,
+  updateAfterChatGPT,
+} from "../Controllers/messageController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
