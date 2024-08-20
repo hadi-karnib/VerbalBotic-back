fa67ed8b116e78dbeb75d9e67ea63c64752710d3
hadi-karnib <hadikarnib03@gmail.com>
2024-08-21 01:54:28 +0300

    added login function

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -35,3 +35,27 @@ export const signup = async (req, res) => {
       .json({ message: "Something went wrong", error: err.message });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = generateToken(user);
+
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong", error: err.message });
+  }
+};
