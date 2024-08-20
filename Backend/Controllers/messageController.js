4c4777a29f349b817bdb8867fbbd888375116ebc
hadi-karnib <hadikarnib03@gmail.com>
2024-08-21 02:32:14 +0300

    added create updateAfterChatGPT function

--- Backend/Controllers/messageController.js
+++ Backend/Controllers/messageController.js
@@ -55,3 +55,32 @@ export const updateAfterAnalysis = async (req, res) => {
       .json({ message: "Failed to update diagnosis", error: err.message });
   }
 };
+
+export const updateAfterChatGPT = async (req, res) => {
+  const { messageId } = req.params;
+  const { AI_response } = req.body;
+
+  try {
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const message = user.chat.messages.id(messageId);
+
+    if (!message) {
+      return res.status(404).json({ message: "Message not found" });
+    }
+
+    message.AI_response = AI_response;
+
+    await user.save();
+
+    res.status(200).json(message);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to update AI response", error: err.message });
+  }
+};
