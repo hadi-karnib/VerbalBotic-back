e3f363ed407818e51d49ce6344636040eee3a208
hadi-karnib <hadikarnib03@gmail.com>
2024-08-21 01:29:10 +0300

    added user schema

---
+++ Backend/Models/User.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import MessageSchema from "./Message.js";
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  illness: {
+    type: String,
+    required: false,
+  },
+  UserType: {
+    type: String,
+    enum: ["parent", "child"],
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  children: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  chat: {
+    messages: {
+      type: [MessageSchema],
+    },
+  },
+});
+
+export default mongoose.model("User", UserSchema);
