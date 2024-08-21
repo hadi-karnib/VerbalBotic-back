d901cbcee2e288d1090b112ae36e208f8fb2b43e
hadi-karnib <hadikarnib03@gmail.com>
2024-08-21 23:40:40 +0300

    added route

--- Backend/Routes/childRoutes.js
+++ Backend/Routes/childRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { addChild, deleteChild } from "../Controllers/childController.js";
+import {
+  addChild,
+  deleteChild,
+  getChildren,
+} from "../Controllers/childController.js";
 import { authMiddleware } from "../Middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/add", authMiddleware, addChild);
+router.get("/", authMiddleware, getChildren);
 
 router.delete("/delete/:childId", authMiddleware, deleteChild);
 
