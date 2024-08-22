de737c9cda8f6b71583b716680f431ebc22a8fc3
hadi-karnib <hadikarnib03@gmail.com>
2024-08-23 00:23:25 +0300

    fixed cors

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
