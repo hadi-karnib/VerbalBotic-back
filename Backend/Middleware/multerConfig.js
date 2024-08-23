8798ae4d931dccaacd3fcbf9dfa46cbe70cc78e1
hadi-karnib <hadikarnib03@gmail.com>
2024-08-23 18:52:10 +0300

    fixed middleware

--- Backend/Middleware/multerConfig.js
+++ Backend/Middleware/multerConfig.js
@@ -1,17 +1,35 @@
 import multer from "multer";
 import path from "path";
 
+// Set storage engine
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, "uploads/"); // Folder where files will be stored
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(
       null,
-      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
     );
   },
 });
 
-export const upload = multer({ storage: storage });
+export const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /wav|mp3|m4a/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Audio Files Only!");
+  }
+}
