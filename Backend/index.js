20e06fcebe80680eebbc5900435241a1ea245eec
hadi-karnib <hadikarnib03@gmail.com>
2024-08-23 22:52:14 +0300

    foxed errors and added /uploads dir

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
 import userRoutes from "./Routes/userRoutes.js";
 import childrenRoutes from "./Routes/childRoutes.js";
 import messageRoutes from "./Routes/messageRoutes.js";
 dotenv.config();
+
 const app = express();
 
 app.use(
@@ -16,6 +19,7 @@ app.use(
   })
 );
 app.use(express.json());
+
 const mongoURI = process.env.MONGO_URI;
 
 mongoose
@@ -23,10 +27,14 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 app.use("/api/user", userRoutes);
 app.use("/api/children", childrenRoutes);
 app.use("/api/messages", messageRoutes);
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
