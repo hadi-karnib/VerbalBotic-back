fe8cacf5306599a048cd327a9188ce97734da986
hadi-karnib <hadikarnib03@gmail.com>
2024-08-25 19:47:08 +0300

    fixed code

--- Backend/Middleware/multerConfig.js
+++ Backend/Middleware/multerConfig.js
@@ -4,7 +4,7 @@ import path from "path";
 // Set storage engine
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/"); // Folder where files will be stored
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
     cb(
