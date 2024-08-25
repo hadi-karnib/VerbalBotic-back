625ecb6a923819ff8ac3026e1414f9f3a0161eed
hadi-karnib <hadikarnib03@gmail.com>
2024-08-26 00:24:14 +0300

    added getself function

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -167,3 +167,17 @@ export const addBio = async (req, res) => {
     });
   }
 };
+export const getSelf = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({
+      message: "Something went wrong",
+      error: err.message,
+    });
+  }
+};
