bae0676a0dfda54b786b0bbcb270e70002d19350
hadi-karnib <hadikarnib03@gmail.com>
2024-08-26 18:05:42 +0300

    added timestamps to user model

--- Backend/Models/User.js
+++ Backend/Models/User.js
@@ -7,78 +7,83 @@ const { Schema } = mongoose;
 const lebanesePhoneRegex =
   /^(?:\+961|961|0)?(81\d{6}|71\d{6}|76\d{6}|79\d{6}|3\d{6})$/;
 
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  illness: {
-    type: String,
-    required: false,
-  },
-  UserType: {
-    type: String,
-    enum: ["parent", "child"],
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  phoneNumber: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: function (v) {
-        return lebanesePhoneRegex.test(v);
-      },
-      message: (props) =>
-        `${props.value} is not a valid Lebanese phone number!`,
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-  },
-  bio: {
-    type: String,
-    required: false,
-  },
-  hobbies: {
-    type: [String],
-    required: false,
-  },
-  work: {
-    type: String,
-    required: false,
-  },
-  children: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  chat: {
-    messages: {
-      type: [MessageSchema],
-      default: [],
-      select: false,
-    },
-    lastLogin: {
-      type: Date,
-      required: false,
-    },
-    streak: {
+    age: {
       type: Number,
+      required: true,
+    },
+    illness: {
+      type: String,
       required: false,
     },
+    UserType: {
+      type: String,
+      enum: ["parent", "child"],
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return lebanesePhoneRegex.test(v);
+        },
+        message: (props) =>
+          `${props.value} is not a valid Lebanese phone number!`,
+      },
+    },
+    bio: {
+      type: String,
+      required: false,
+    },
+    hobbies: {
+      type: [String],
+      required: false,
+    },
+    work: {
+      type: String,
+      required: false,
+    },
+    children: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    chat: {
+      messages: {
+        type: [MessageSchema],
+        default: [],
+        select: false,
+      },
+      lastLogin: {
+        type: Date,
+        required: false,
+      },
+      streak: {
+        type: Number,
+        required: false,
+      },
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("User", UserSchema);
