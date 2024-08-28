9fd7ee9a4c23124f70418d5e50443ee535352fd7
hadi-karnib <hadikarnib03@gmail.com>
2024-08-28 23:30:12 +0300

    added fastapi back

--- ML/app.py
+++ ML/app.py
@@ -1,3 +1,23 @@
 from fastapi import FastAPI, UploadFile, File
 from ML.model.wav2vec2 import SpeechToTextModel
 import os
+
+
+app = FastAPI()
+
+model = SpeechToTextModel()
+
+@app.post("/transcribe/")
+async def transcribe_audio(file: UploadFile = File(...)):
+    # Save the uploaded file temporarily
+    file_location = f"temp_{file.filename}"
+    with open(file_location, "wb+") as file_object:
+        file_object.write(file.file.read())
+    
+    # Transcribe the audio
+    transcription = model.transcribe(file_location)
+    
+    # Remove the temp file
+    os.remove(file_location)
+    
+    return {"transcription": transcription}
