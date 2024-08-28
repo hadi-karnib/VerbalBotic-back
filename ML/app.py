0ddb860143d2ea119500c0425886fbb2ea7244b1
hadi-karnib <hadikarnib03@gmail.com>
2024-08-28 23:32:03 +0300

    refactored code

--- ML/app.py
+++ ML/app.py
@@ -9,15 +9,12 @@ model = SpeechToTextModel()
 
 @app.post("/transcribe/")
 async def transcribe_audio(file: UploadFile = File(...)):
-    # Save the uploaded file temporarily
     file_location = f"temp_{file.filename}"
     with open(file_location, "wb+") as file_object:
         file_object.write(file.file.read())
     
-    # Transcribe the audio
     transcription = model.transcribe(file_location)
     
-    # Remove the temp file
     os.remove(file_location)
     
     return {"transcription": transcription}
