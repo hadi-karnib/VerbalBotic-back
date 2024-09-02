ea3b0eb6496edb563ce5d4811edf6aa47561a8f7
hadi-karnib <hadikarnib03@gmail.com>
2024-09-02 19:27:27 +0300

    added route to fetch children chats

--- Backend/Routes/messageRoutes.js
+++ Backend/Routes/messageRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createVoiceNote,
+  fetchChildChats,
   getMyChats,
   transcribeAudioGoogle,
   updateAfterAnalysis,
@@ -14,7 +15,7 @@ router.post("/", authMiddleware, createVoiceNote);
 router.post("/transcribeGoogle", authMiddleware, transcribeAudioGoogle);
 
 router.get("/", authMiddleware, getMyChats);
-
+router.post("/child-chats", authMiddleware, fetchChildChats);
 router.patch("/:messageId/analysis", authMiddleware, updateAfterAnalysis);
 router.patch("/:messageId/chatgpt", authMiddleware, updateAfterChatGPT);
 
