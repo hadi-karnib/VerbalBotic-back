4b4494e6458095792fe623c153c0db4baa075de4
hadi-karnib <hadikarnib03@gmail.com>
2024-09-04 17:38:43 +0300

    created the getAdvice function

--- Backend/CHAT_GPT/ParentAdvice.js
+++ Backend/CHAT_GPT/ParentAdvice.js
@@ -2,3 +2,7 @@ import axios from "axios";
 
 const CHATGPT_API = process.env.CHATGPT_API;
 const CHAT_MODEL = process.env.CHAT_MODEL || "gpt-3.5-turbo";
+
+const getAdvice = async (prompt) => {};
+
+export default getAdvice;
