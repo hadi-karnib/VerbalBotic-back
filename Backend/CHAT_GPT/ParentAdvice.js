34b6eafb8008e144fa877cb99243c9b853d3b2ae
hadi-karnib <hadikarnib03@gmail.com>
2024-09-04 17:39:04 +0300

    renamed to getParentAdvice

--- Backend/CHAT_GPT/ParentAdvice.js
+++ Backend/CHAT_GPT/ParentAdvice.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const CHATGPT_API = process.env.CHATGPT_API;
 const CHAT_MODEL = process.env.CHAT_MODEL || "gpt-3.5-turbo";
 
-const getAdvice = async (prompt) => {};
+const getParentAdvice = async (prompt) => {};
 
 export default getAdvice;
